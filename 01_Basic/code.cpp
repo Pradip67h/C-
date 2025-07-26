38c47413a86545f2b0a73a01080e0ab7de3de622
Pradip Hazra <codedot12@gmail.com>
2025-07-26 10:58:12 +0000

    c++ first code

---
+++ 01_Basic/code.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello Pradip\nFrom Haldia\n";
+    int age = 20;
+    cout << age << endl;
+    cout << sizeof(age) << endl;      // 4 bytes
+    char grade = 'A';
+    cout << grade << endl;
+    float PI = 3.14f;
+    cout << PI << endl;
+    bool isSafe = false;
+    cout << isSafe << endl;     // 0:- False, 1:- True
+    double price = 100.99;
+    cout << price << endl;
+    return 0;
+}
+
+// This is single line comment
+/*  this is 
+      multiple line 
+        comment  */
+
+      
