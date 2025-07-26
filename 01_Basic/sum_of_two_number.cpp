29e37fb0d3e35bec91093a2b2b8456d9a8c24aec
Pradip Hazra <codedot12@gmail.com>
2025-07-26 06:17:30 -0930

    Create sum_of_two_number.cpp
    Sum of Two Number in user input

---
+++ 01_Basic/sum_of_two_number.cpp
@@ -0,0 +1,14 @@
+// ++++++++++++++++ Sum of 2 Number In User Input ++++++++++++++
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a,b;
+    cout << "Enter your first Number : ";
+    cin >> a;
+    cout << "Enter your second Number : ";
+    cin >> b;
+    cout << "Sum of the Numebr : " << (a+b) << endl;
+    return 0;
+}
