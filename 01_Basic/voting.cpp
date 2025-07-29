fe4304c43696a127caae01f7cde897c4cbd3fcf4
Pradip Hazra <codedot12@gmail.com>
2025-07-29 02:59:12 -0930

    Create voting.cpp
    normal voting process in cpp

---
+++ 01_Basic/voting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 12;
+
+    if(n>=18){
+        cout << "You are vote!\n";
+    } else {
+        cout << "You are not eligible for the vote!\n";
+    }
+    return 0;
+}
