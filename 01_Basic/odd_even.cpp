fdf651920e3e08f9afdca000444b8798ad8d8d74
Pradip Hazra <codedot12@gmail.com>
2025-07-29 03:13:28 -0930

    Create odd_even.cpp
    normal odd and even system in cpp

---
+++ 01_Basic/odd_even.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter your Number : ";
+    cin >> n;
+    if(n%2==0){
+       cout << "Number is Even\n";
+    } else{
+        cout << "Number is Odd\n";
+    }
+}
