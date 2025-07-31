71ebffb525ed3b2fb1cf57fb9de71ec6bc11a2ad
Pradip Hazra <codedot12@gmail.com>
2025-07-30 22:07:49 -0930

    Create nested_loops.cpp
    nested loops using cpp

---
+++ 01_Basic/nested_loops.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+    for (int i=1; i<=n; i++){   // Number of Line
+        int x = 7;
+        for(int j=1; j<=x; j++) {  
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
