7702b35325b03e97bde847f97787d8d572802925
Pradip Hazra <codedot12@gmail.com>
2025-08-01 06:21:14 -0930

    Create floyds_triangle_pattern_with_character.cpp
    floyd's triangle pattern with character in cpp

---
+++ 02_Basic/floyds_triangle_pattern_with_character.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+  int main() {
+  int n;
+  char ch = 'A';
+    cout << "Enter a positive Number(n) : ";
+    cin >> n;
+
+    if(n<0){
+        cout << "Error! please enter a positive number.";
+    } else {
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j>0; j--){  
+                cout << ch << " ";
+                ch++;
+            }
+            cout << endl;
+        }
+        return 0;
+    } 
+}
