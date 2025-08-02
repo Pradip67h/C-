9d332c965537353b10ab54358279c4d1c614278b
Pradip Hazra <codedot12@gmail.com>
2025-08-01 18:56:29 -0930

    Create sum_of_2_number.cpp
    sum of 2 number using function in cpp

---
+++ Function/sum_of_2_number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// sum of 2 numbers
+double sum(double a, double b){
+    double s = a + b;
+    return s;
+}
+
+int main() {
+    cout << sum(10.99, 5.87) << endl;
+
+    return 0;
+}
