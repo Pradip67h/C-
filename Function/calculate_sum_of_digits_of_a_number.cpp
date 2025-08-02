02b4a138816f82b020f7d7c624c80174fbcb99dc
Pradip Hazra <codedot12@gmail.com>
2025-08-02 05:03:52 -0930

    Create calculate_sum_of_digits_of_a_number.cpp
    calculate sum of digits of a number in cpp

---
+++ Function/calculate_sum_of_digits_of_a_number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int sumOfDigits(int num) {
+    int digSum = 0;
+
+    while (num > 0)
+    {
+        int lastDig = num % 10;
+        num /= 10;
+
+        digSum += lastDig;
+    }
+    
+    return digSum;
+}
+
+int main() {
+    cout << "sum = " << sumOfDigits(1234) << endl;
+    cout << "sum = " << sumOfDigits(6554) << endl;
+
+    return 0;
+}
