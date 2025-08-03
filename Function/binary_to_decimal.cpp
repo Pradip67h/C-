41712d019d367abf66e04a827b77effc77b3c25a
Pradip Hazra <codedot12@gmail.com>
2025-08-02 18:47:38 -0930

    Create binary_to_decimal.cpp
    binary to decimal number system in cpp

---
+++ Function/binary_to_decimal.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int binToDecimal(int binNum) {
+    int ans = 0, pow = 1;
+    
+    while(binNum > 0){
+        int rem = binNum % 10;
+        ans += rem * pow;
+
+        binNum /= 10;
+        pow *= 2;
+    }
+    return ans; // decimal form
+}
+
+int main() {
+    int binNum = 1100101;
+
+    cout << binNum << " Binary to Decimal = " << binToDecimal(binNum) << endl;
+
+    return 0;
+}
