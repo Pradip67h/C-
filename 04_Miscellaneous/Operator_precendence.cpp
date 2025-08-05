67a6669f2537b97202e2396394201266a18f621b
Pradip Hazra <codedot12@gmail.com>
2025-08-05 05:36:47 -0930

    Create Operator_precendence.cpp
    Operator precendence in cpp

---
+++ 04_Miscellaneous/Operator_precendence.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int result = 10 + 20 * 30;
+
+    int res = 100 / 10 *10;
+
+    int resu = 100 + 200 / 10 - 3 * 10;
+    
+    
+    cout << "Result: " << resu << endl;   
+    
+    cout<<"Result: "<< res<<"\n";
+    
+
+    cout << "Result: " << result << endl;
+
+    cout << "RESULT : " << (5-3)*10 << endl;      // 20
+    cout << "RESULT : " << 5-3*10 << endl;        // -25
+
+    cout << "Result : " <<  4*5%3 << endl;        //  rem : 2
+
+    return 0;
+}
