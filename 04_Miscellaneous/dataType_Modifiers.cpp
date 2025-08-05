94e6528c0dd5c39e2234aeecc4974cffb278a54e
Pradip Hazra <codedot12@gmail.com>
2025-08-05 06:17:12 -0930

    Create dataType_Modifiers.cpp
    Data Type Modifiers in cpp

---
+++ 04_Miscellaneous/dataType_Modifiers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    unsigned int x = 10;
+    unsigned int y = -10;
+
+    cout << x << endl;        //  10
+    cout << y << endl;       // -10 = 4294967286
+
+    cout << sizeof(int) << endl;
+
+    cout << sizeof(long int) << endl;
+
+    cout << sizeof(short int) << endl;
+
+     cout << sizeof(long long) << endl;  // long long int :- long long 
+
+    return 0;
+}
