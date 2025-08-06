c6d3816e51ee46b6260d7b440f2d3fa68ac6e02a
Pradip Hazra <codedot12@gmail.com>
2025-08-06 05:15:24 -0930

    Create pass_by_reference.cpp
    pass by reference in cpp

---
+++ 05_Array/pass_by_reference.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void changeArr(int arr[], int size) {
+    cout << "in function\n";
+    for(int i=0; i<size; i++) {
+        arr[i] = 2* arr[i];
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4};
+
+    changeArr(arr, 4);
+
+    cout << "in main\n";
+     for(int i=0; i<4; i++) {  // 2 4 6 8 
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
