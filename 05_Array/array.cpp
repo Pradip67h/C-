109393204fd8032b4686401d114cfaa728bbb133
Pradip Hazra <codedot12@gmail.com>
2025-08-05 22:40:51 -0930

    Create array.cpp
    array in cpp

---
+++ 05_Array/array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks[5] = {99, 100, 54, 36, 88};
+    marks[0] = 102;
+
+  //  double price[] = {98.99, 86.92, 98.77};
+
+    cout << marks[0] << endl;
+    cout << marks[1] << endl;
+    cout << marks[2] << endl;
+    cout << marks[3] << endl;
+    cout << marks[4] << endl;
+
+    // 0 to size-1
+    
+    return 0;
+}
