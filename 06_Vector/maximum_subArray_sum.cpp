3821484773b9a41e3e1d6262c340466674932b33
Pradip Hazra <codedot12@gmail.com>
2025-08-19 05:32:49 -0930

    Create maximum_subArray_sum.cpp
    maximum sub array sum in cpp

---
+++ 06_Vector/maximum_subArray_sum.cpp
@@ -0,0 +1,24 @@
+// Maximum subarray sum 
+#include <iostream>
+#include <climits>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n = 7;
+    int arr[7] = {3, -4, 5, 4, -1, 7, -8};
+
+    int maxSum = INT_MIN;
+
+    for (int st = 0; st < n; st++) {
+        int currSum = 0;
+        for (int end = st; end < n; end++) {
+            currSum += arr[end];
+            maxSum = max(currSum, maxSum);
+        }
+    }
+
+    cout << "max subarray sum = " << maxSum << endl;
+
+    return 0;
+}
